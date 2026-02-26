792760f9945e5bd767d0a0155306027e2fb8e625
pulba <pulbagrup@gmail.com>
2026-02-26 16:04:37 +0700

    fix(tina): add CF_PAGES_BRANCH for cloudflare pages branch detection

--- tina/config.ts
+++ tina/config.ts
@@ -7,6 +7,7 @@ const branch =
     process.env.GITHUB_BRANCH ||
     process.env.VERCEL_GIT_COMMIT_REF ||
     process.env.HEAD ||
+    process.env.CF_PAGES_BRANCH ||
     "main";
 
 export default defineConfig({
